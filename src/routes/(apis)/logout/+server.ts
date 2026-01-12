c103e03ff2e4fd99e773ab30b310d79dbd149c9d
Mohammed Alnahhal <79056491+mohamedalnahhal@users.noreply.github.com>
2026-01-12 13:52:57 +0200

    Delete jwt cookie when logout

--- src/routes/(apis)/logout/+server.ts
+++ src/routes/(apis)/logout/+server.ts
@@ -1,3 +1,4 @@
+import { jwtExpirationSeconds } from "$lib/server/auth/jwt";
 import { deleteSession, maxSessionSeconds } from "$lib/server/auth/session.js";
 import { redirect } from "@sveltejs/kit";
 
@@ -5,6 +6,7 @@ export async function GET({ url, cookies, locals }) {
   if (locals.session) {
     await deleteSession(locals.session.id);
     cookies.delete("session_token", { path: '/', httpOnly: true, maxAge: maxSessionSeconds, secure: true, sameSite: 'lax' });
+    cookies.delete("session_jwt", { path: '/', httpOnly: true, maxAge: jwtExpirationSeconds, secure: true, sameSite: 'lax' });
     locals.session = null;
   }
   if (url.searchParams.has('continue')) {
